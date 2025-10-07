2c8ce99244bdcb872d87b0cfede627eee462d122
Duvan <duvangiraldo124@gmail.com>
2025-10-07 18:43:05 -0500

    rest server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
 
     port:number
     public_path:string;
+    routes: Router;
 
 }
 
@@ -14,19 +15,33 @@ export class Server {
     private app = express();
     private readonly port:number;
     private readonly publicPath:string;
+    private readonly routes: Router;
 
     constructor(options: Options){
 
-        const {port, public_path} = options;
+        const {port, public_path, routes} = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
 
     }
 
 
     async start(){
 
+        // Middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+
         this.app.use(express.static(this.publicPath));
+
+
+        // Routes
+        this.app.use(this.routes);
+        
+
+        // SPA
+
 
         this.app.get('/*splat',(req, res) => {
 
